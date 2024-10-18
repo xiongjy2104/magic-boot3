d7cabcbf7ed7de2b19102debbc3a014176e7d707
xiongjy2104 <xiongjy2104@gmail.com>
2024-10-18 17:38:39 +0800

    add config

--- src/main/java/org/ssssssss/magicboot/MagicBootApplication.java
+++ src/main/java/org/ssssssss/magicboot/MagicBootApplication.java
@@ -1,6 +1,5 @@
 package org.ssssssss.magicboot;
 
-//import com.github.netty.springboot.EnableNettyEmbedded;
 import com.github.netty.springboot.EnableNettyEmbedded;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -8,21 +7,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.ssssssss.magicapi.core.config.MagicAPIProperties;
 
 import java.util.Arrays;
-
-//@SpringBootApplication
-//public class MagicBootApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(MagicBootApplication.class, args);
-//    }
-//
-//}
-
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableNettyEmbedded
  @EnableWebSocket // 如果引入了websocket，可以打这个注解开启
@@ -36,13 +27,14 @@ public class MagicBootApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(MagicBootApplication.class, args);
-
-        String[] array = applicationContext.getBeanDefinitionNames();
-        Arrays.sort(array);
-        System.out.println("系统已加载的beanName列表：");
-        for (String beanName : array) {
-            System.out.printf("    %s\n", beanName);
-        }
+//        if(magicAPIProperties.showBeans()) {
+//            String[] array = applicationContext.getBeanDefinitionNames();
+//            Arrays.sort(array);
+//            System.out.println("系统已加载的beanName列表：");
+//            for (String beanName : array) {
+//                System.out.printf("    %s\n", beanName);
+//            }
+//        }
     }
 
     @Override
